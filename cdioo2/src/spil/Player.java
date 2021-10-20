7bd5d6c7ddc2e44f8896cbee7d3cba298696dd79
Sebastian Ellegaard <sebynoah@gmail.com>
2021-10-20 14:41:45 +0200

    Player class updated

--- cdioo2/src/spil/Player.java
+++ cdioo2/src/spil/Player.java
@@ -1,4 +1,21 @@
 package spil;
 
 public class Player {
+
+    private String name;
+    private Account account;
+
+    public Player(String name, Account account){
+        this.name =name;
+        this.account =account;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
 }
