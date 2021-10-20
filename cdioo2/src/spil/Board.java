58095bf2827f3df0b475dd56164254fe1b96b213
KaNN <lasseg123@hotmail.com>
2021-10-20 13:21:14 +0200

    New class spaces under board, with constructor

--- cdioo2/src/spil/Board.java
+++ cdioo2/src/spil/Board.java
@@ -3,3 +3,14 @@ package spil;
 public class Board {
 
 }
+
+class Spaces{
+
+    int value;
+    String output;
+
+    Spaces(int valuePass, String outputPass){
+        this.value = valuePass;
+        this.output = outputPass;
+    }
+}
