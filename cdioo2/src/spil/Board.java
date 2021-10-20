243ecd30ff39f7513067f93782a4447374134bcd
Valdemar Nielsen <v.nielsen@live.dk>
2021-10-20 14:01:01 +0200

    Output text written and spacevalues made.

--- cdioo2/src/spil/Board.java
+++ cdioo2/src/spil/Board.java
@@ -1,16 +1,11 @@
 package spil;
 
 public class Board {
+    public static void values(String[] args) {
+        int[] spaces;
+        spaces = new int[12];
+        int[] spacevalues = {250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
 
-}
 
-class Space{
-
-    int value;
-    String output;
-
-    Space(int valuePass, String outputPass){
-        this.value = valuePass;
-        this.output = outputPass;
     }
 }
--- cdioo2/src/spil/Output.java
+++ cdioo2/src/spil/Output.java
@@ -1,4 +1,21 @@
 package spil;
 
 public class Output {
-}
+    // Insert output for each board space.
+
+    String[] spacetext = {
+            "You found a chest inside the Tower! You have been rewarded with 250 points",
+            "You landed in the Crater, and dropped 100 points on the way",
+            "Upon arriving at the Palace Gates, you received 100 points",
+            "You're no match for the Cold Desert, and to survive you pay 20 points for shelter",
+            "The Walled City rarely gets visitors, and grants you 180 points for your stay",
+            "The Monastery is a sacred place, and do not believe in material goods, therefore you neither lose or obtain points",
+            "This Black Cave you've entered is no place for a stranger, and you lose 70 points while you're shivering uncontrollably",
+            "These old Huts in the mountains are abandoned, you but find a total of 60 points playing around for the taking",
+            "The Werewall (werewolf-wall) claims 80 of your points, however in pity for such horrors, you are granted another roll",
+            "The pit takes what is wants, and you lose 50 points",
+            "You found gold in the mountains and sell it for 650 points. You're rich!"};
+    }
+
+
+
