1e06e0f4e14bbf441a0ef9d4c82b9adca7897a74
Sebastian <Sig.goldbach@gmail.com>
2021-10-20 12:49:25 +0200

    Første version af terning

--- cdioo2/src/spil/Die.java
+++ cdioo2/src/spil/Die.java
@@ -1,7 +1,31 @@
 package spil;
 
 public class Die {
-    private int vaeerdi;
-    private int antalsider;
-}
+    private int value;
+    private  int faceAmount;
 
+    public Die(int faceAmount) {
+        this.faceAmount=faceAmount;
+        this.value=1;
+    }
+    public void setFaceAmount(int faceAmount) {
+        this.faceAmount = faceAmount;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getFaceAmount() {
+        return faceAmount;
+    }
+    public void roll(){
+        value=(int) (Math.random()*faceAmount+1);
+    }
+
+
+}
