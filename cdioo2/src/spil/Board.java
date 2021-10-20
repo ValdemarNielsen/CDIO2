ba28ad8a8b7825663c5d0b05d5797f262ae6d65a
KaNN <lasseg123@hotmail.com>
2021-10-20 14:25:14 +0200

    Setup board generation and output text getter

--- cdioo2/src/spil/Board.java
+++ cdioo2/src/spil/Board.java
@@ -1,12 +1,16 @@
 package spil;
 
 public class Board {
-    public static void values(String[] args) {
-        int[] spaces;
-        spaces = new int[12];
-        int[] spacevalues = {250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
 
+    Space[] spaces;
+    int[] spaceValues = {250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
 
+    String[] spaceText = Output.spacetextGetter();
+
+    public void generateBoard(){
+        for(int i = 0; i < 11; i++){
+            spaces[i] = new Space(spaceValues[i + 2], spaceText[i + 2]);
+        }
     }
 }
 
--- cdioo2/src/spil/Output.java
+++ cdioo2/src/spil/Output.java
@@ -1,9 +1,8 @@
 package spil;
 
 public class Output {
-    // Insert output for each board space.
 
-    String[] spacetext = {
+    private static String[] spacetext = {
             "You found a chest inside the Tower! You have been rewarded with 250 points",
             "You landed in the Crater, and dropped 100 points on the way",
             "Upon arriving at the Palace Gates, you received 100 points",
@@ -15,6 +14,12 @@ public class Output {
             "The Werewall (werewolf-wall) claims 80 of your points, however in pity for such horrors, you are granted another roll",
             "The pit takes what is wants, and you lose 50 points",
             "You found gold in the mountains and sell it for 650 points. You're rich!"};
+
+    // Returns the spacetext
+    public static String[] spacetextGetter(){
+        return spacetext;
+    }
+
     }
 
 
